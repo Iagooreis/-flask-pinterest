428f065ffb6447bfea92c29a5e8065d7f0c8f7c3
iagooreis <iagooreis27@hotmail.com>
2025-03-11 21:11:50 -0300

    commit - adicionando os arquivos do projeto

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "e3a1f5b6c7d8e9f0a1b2c3d4e5f60708"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
+
+
+
+from fakepinterest import routes
